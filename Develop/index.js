060a41822170bf7f72d6b52cc075d1b9cc2c0c25
Your Name <felix.acevedo.tech@gmail.com>
2022-03-26 11:42:37 -0400

    working on promises for question function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,133 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptQuestions = readmeData => {
+    if(!readmeData.projects){
+        readmeData.projects = [];
+    }
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project title (Required)?',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required).',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    console.log('Here is a guide: ');
+                    console.log('What was your motivation?');
+                    console.log('Why did you build this project? (Note: the answer is not "Because it was a homework assignment.)');
+                    console.log('What problem does it solve?');
+                    console.log('What did you learn?');
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project (Required)? Provide a step-by-step description of how to get the development environment running.',
+            validate: installationInput => {
+                if(installationInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your steps for installation!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the instructions and examples for use (Required)?',
+            validate: usageInput => {
+                if(usageInput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions and examples for use!');
+                    return false;
+                }
+            }
+        },
+        // {
+        //     type: 'confirm',
+        //     name: 'confirmContribution',
+        //     message: 'Would you like to enter some info on how other developers can contribute?',
+        //     default: true
+        // },
+        // {
+        //     type: 'input',
+        //     name: 'contribution',
+        //     message: 'Please provide Contribution Info.',
+        //     when: ({ confirmContribution }) => confirmContribution
+        // }
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter some info on how other developers can contribute.',
+            validate: contributionInput => {
+              if (contributionInput) {
+                return true;
+              } else {
+                console.log('Since you have not entered your own contribution info, we will include the Contribution Covenant.');
+                // area for inserting link or text from the Contribution Covenant
+                return false;
+              }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'test',
+            message: 'Would you like to test your knowledge?',
+            default: false,
+            if (test){
+                //test questions
+
+            }
+        }, 
+    ])
+    .then(projectData => 
+        readmeData.projects.push(projectData));
+        // return readmeData;
+        
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeToFile('./dist/README.md', fileContent, err => {
+            if(err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File created!'
+            })
+        });
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+promptQuestions();
